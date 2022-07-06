134ebedc1d2a84ad576399b8deb8d3c30b0fbce5
Shmuel Hoffman <snyh212@gmail.com>
2022-07-05 22:03:18 -0700

    working on js

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var search = $("#search")
+var cityInput = $("#cityInput");
+
+function blue(){
+    var list = document.querySelector('#cityInput').value;
+    var txt = document.querySelector('#pSearch1').createTextNode(list);
+}
+ blue();
