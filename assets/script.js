5367cb8aaf64a36dd948912544d0703c410a0dc5
Shmuel Hoffman <snyh212@gmail.com>
2022-07-06 23:09:00 -0700

    changed from ajax to fetch, new second link and got info from objects put in html

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 //only runs ones page loaded
 $(document).ready(function(){
 
-
 var search = $("#search")
 var cityInput = $("#cityInput").html;
 
@@ -10,7 +9,41 @@ search.click(function() {
     var cInput = $('#cityInput').val();
     $("ul").append("<li></li>").text(cInput)
 
+    var url1 = "http://api.openweathermap.org/geo/1.0/direct?q="+cInput+"&limit=1&appid=50264aa9ddadbf6ae7e3c2c0f0352af2"
+    fetch(url1).then(function(response) {
+        return response.json();
+    }).then (function (data) {
+        console.log(data);
+
+        var lat = data.list;
+        var lon = data.list;
+
+        console.log(lat);
+        console.log(lon);
+
+
+        var url2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cInput + "&units=imperial&appid=50264aa9ddadbf6ae7e3c2c0f0352af2";
+        fetch(url2).then(function(response) {
+            return response.json();
+        }).then (function (data) {
+            console.log(data);
+
+            var cityName = data.city.name;
+            var temperature = data.list[0].main.temp + " °F";
+            var wind = data.list[0].wind.speed + " MPH";
+            var humidity = data.list[0].main.humidity + " %";
+
+            console.log(cityName);
+            console.log(temperature);
+            console.log(wind);
+            console.log(humidity);
+
+            $("h2").text(cityName)
+            $("#temp").text(temperature)
+            $("#wind").text(wind)
+            $("#humidity").text(humidity)
+        });
+    });
 });
-    
-    
+
 });
